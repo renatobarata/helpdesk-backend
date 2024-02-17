7ce7b359b2f35a99f541378290b84fab0d35a80b
renatobarata <renatobaratagomes@gmail.com>
2024-02-17 15:52:59 -0300

    projeto iniciado e classes de dominio criadas

---
+++ src/test/java/com/valdir/helpdeskbackend/HelpdeskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valdir.helpdeskbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
