1176173a0df2c6146c12affb4b4004a21c4d87e4
renatobarata <renatobaratagomes@gmail.com>
2024-02-18 18:17:36 -0300

    Metodo DELETE para tecnico implementado.

--- src/main/java/com/valdir/helpdeskbackend/services/TecnicoService.java
+++ src/main/java/com/valdir/helpdeskbackend/services/TecnicoService.java
@@ -49,6 +49,16 @@ public class TecnicoService {
 		return repository.save(oldObj);
 	}
 	
+	public void delete(Integer id) {
+		Tecnico obj = findById(id);
+		
+		if(obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Técnico possui ordens de serviço e não pode ser deletado.");
+		}
+		
+		repository.deleteById(id);
+	}
+	
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
 		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
@@ -61,5 +71,4 @@ public class TecnicoService {
 		}
 	}
 
-	
 }
