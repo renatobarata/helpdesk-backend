e72ef5ddb344148c892c366e7914d45d86c5ab3a
renatobarata <renatobaratagomes@gmail.com>
2024-02-18 19:19:37 -0300

    Create para Chamado.

--- src/main/java/com/valdir/helpdeskbackend/domain/dtos/ChamadoDTO.java
+++ src/main/java/com/valdir/helpdeskbackend/domain/dtos/ChamadoDTO.java
@@ -3,6 +3,8 @@ package com.valdir.helpdeskbackend.domain.dtos;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.valdir.helpdeskbackend.domain.Chamado;
 
@@ -18,16 +20,22 @@ public class ChamadoDTO implements Serializable {
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
 	
+	@NotNull(message = "O campo prioridade é requerido.")
 	private Integer prioridade;
 	
+	@NotNull(message = "O campo status é requerido.")
 	private Integer status;
 	
+	@NotNull(message = "O campo título é requerido.")
 	private String titulo;
 	
+	@NotNull(message = "O campo observações é requerido.")
 	private String observacoes;
 	
+	@NotNull(message = "O campo técnico é requerido.")
 	private Integer tecnico;
 	
+	@NotNull(message = "O campo cliente é requerido.")
 	private Integer cliente;
 	
 	private String nomeTecnico;
--- src/main/java/com/valdir/helpdeskbackend/services/ChamadoService.java
+++ src/main/java/com/valdir/helpdeskbackend/services/ChamadoService.java
@@ -3,10 +3,17 @@ package com.valdir.helpdeskbackend.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.valdir.helpdeskbackend.domain.Chamado;
+import com.valdir.helpdeskbackend.domain.Cliente;
+import com.valdir.helpdeskbackend.domain.Tecnico;
+import com.valdir.helpdeskbackend.domain.dtos.ChamadoDTO;
+import com.valdir.helpdeskbackend.domain.enums.Prioridade;
+import com.valdir.helpdeskbackend.domain.enums.Status;
 import com.valdir.helpdeskbackend.repositories.ChamadoRepository;
 import com.valdir.helpdeskbackend.services.exceptions.ObjectNotFoundException;
 
@@ -16,6 +23,12 @@ public class ChamadoService {
 	@Autowired
 	private ChamadoRepository repository;
 	
+	@Autowired
+	private TecnicoService tecnicoService;
+	
+	@Autowired
+	private ClienteService clienteService;
+	
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado. Id: " + id));
@@ -23,5 +36,27 @@ public class ChamadoService {
 
 	public List<Chamado> findAll() {
 		return repository.findAll();
+	}
+
+	public Chamado create(@Valid ChamadoDTO objDTO) {
+		return repository.save(newChamado(objDTO));
+	}
+	
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+		Chamado chamado = new Chamado();
+		
+		if(obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+		
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		return chamado;
 	}
 }
