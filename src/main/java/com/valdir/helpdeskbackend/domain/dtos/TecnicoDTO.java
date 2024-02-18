77e3dddb17ce39837ffdd4c60582bfd825773716
renatobarata <renatobaratagomes@gmail.com>
2024-02-18 16:16:19 -0300

    Validacao nos campos de criacao de novo usuario.

--- src/main/java/com/valdir/helpdeskbackend/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/valdir/helpdeskbackend/domain/dtos/TecnicoDTO.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.valdir.helpdeskbackend.domain.Tecnico;
 import com.valdir.helpdeskbackend.domain.enums.Perfil;
@@ -15,10 +17,19 @@ public class TecnicoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	protected Integer id;
+	
+	@NotNull(message = "O campos nome é requerido.")
 	protected String nome;
+	
+	@NotNull(message = "O campos CPF é requerido.")
 	protected String cpf;
+	
+	@NotNull(message = "O campos e-mail é requerido.")
 	protected String email;
+	
+	@NotNull(message = "O campos senha é requerido.")
 	protected String senha;
+	
 	protected Set<Integer> perfis = new HashSet<>();
 	
 	@JsonFormat(pattern = "dd/MM/yyyy")
