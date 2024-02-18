c5ad06efbc9603fcf941ae400da476e0f9f069a6
renatobarata <renatobaratagomes@gmail.com>
2024-02-18 16:28:47 -0300

    Metodo PUT para tecnico implementado.

--- src/main/java/com/valdir/helpdeskbackend/services/TecnicoService.java
+++ src/main/java/com/valdir/helpdeskbackend/services/TecnicoService.java
@@ -3,6 +3,8 @@ package com.valdir.helpdeskbackend.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -39,6 +41,14 @@ public class TecnicoService {
 		return repository.save(newObj);
 	}
 
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDto) {
+		objDto.setId(id);
+		Tecnico oldObj = findById(id);
+		validaPorCpfEEmail(objDto);
+		oldObj = new Tecnico(objDto);
+		return repository.save(oldObj);
+	}
+	
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
 		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
@@ -50,4 +60,6 @@ public class TecnicoService {
 			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema.");
 		}
 	}
+
+	
 }
